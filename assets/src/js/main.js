b921d74e2956770a7ea4e80327ba47ca3cc19d2a
chernushov88@gmail.com <chernushov88@gmail.com>
2023-04-27 23:55:13 +0100

    layout section__form

--- assets/src/js/main.js
+++ assets/src/js/main.js
@@ -8,27 +8,27 @@
 
 
 // Импортируем необходимые js-файлы Bootstrap 4
-//= ../../../node_modules/bootstrap/js/dist/util.js
+// ../../../node_modules/bootstrap/js/dist/util.js
 // ../../../node_modules/bootstrap/js/dist/alert.js
 // ../../../node_modules/bootstrap/js/dist/button.js
 // ../../../node_modules/bootstrap/js/dist/carousel.js
 // ../../../node_modules/bootstrap/js/dist/collapse.js
 // ../../../node_modules/bootstrap/js/dist/dropdown.js
-//= ../../../node_modules/bootstrap/js/dist/modal.js
+// ../../../node_modules/bootstrap/js/dist/modal.js
 // ../. ./../node_modules/bootstrap/js/dist/tooltip.js
 // ../../../node_modules/bootstrap/js/dist/popover.js
 // ../../../node_modules/bootstrap/js/dist/scrollspy.js
-//= ../../../node_modules/bootstrap/js/dist/tab.js
+// ../../../node_modules/bootstrap/js/dist/tab.js
 // ../../../node_modules/bootstrap/js/dist/toast.js
-//= ../../../node_modules/bootstrap-tagsinput/dist/bootstrap-tagsinput.min.js
-//= ../../../node_modules/select-picker/dist/picker.min.js
+// ../../../node_modules/bootstrap-tagsinput/dist/bootstrap-tagsinput.min.js
+// ../../../node_modules/select-picker/dist/picker.min.js
 
 //  Импортируем другие js-файлы
 //   jquery.maskedinput/jquery.maskedinput.min.js
-//=   owl.carousel/owl.carousel.min.js
+//   owl.carousel/owl.carousel.min.js
 //   jquery.fancybox/jquery.fancybox.min.js
-//=   jquery.responsivegrid.js
-//=   jquery-ui.min.js
+//   jquery.responsivegrid.js
+//   jquery-ui.min.js
 //  ../../../node_modules/slick-carousel/slick/slick.min.js
 
 
